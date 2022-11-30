6350fdcf877c9e134c45b464c6c7369b72a1b8b9
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2022-11-30 22:52:32 +0530

    Update homepage.dart

--- mytag/lib/homepage.dart
+++ mytag/lib/homepage.dart
@@ -1,1 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyTag',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final List<Page> _pages = [
+    Page('Home', Icons.home),
+    Page('Profile', Icons.person_outline),
+  ];
+
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentPageIndex = 0;
+
+  void _openPage(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> drawerItemWidgets = widget._pages
+        .asMap()
+        .map((int index, Page page) => MapEntry<int, Widget>(
+            index,
+            ListTile(
+              title: Text(page.title),
+              leading: Icon(page.iconData),
+              selected: _currentPageIndex == index,
+              onTap: () {
+                _openPage(index);
+                Navigator.pop(context);
+              },
+            )))
+        .values
+        .toList();
+    drawerItemWidgets.insert(
+      0,
+      DrawerHeader(
+        child: Text('MyTag'),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+        ),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyTag"),
+      ),
+      body: Center(
+        child: Text(widget._pages[_currentPageIndex].title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: drawerItemWidgets,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        items: widget._pages
+            .map((Page page) => BottomNavigationBarItem(
+                  icon: Icon(page.iconData),
+                  label: page.title,
+                ))
+            .toList(),
+        onTap: _openPage,
+      ),
+      floatingActionButton: Wrap(
+        direction: Axis.vertical,
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.all(10),
+              child: FloatingActionButton(
+                onPressed: () {
+                  //action code for button 1
+                },
+                child: Text(
+                  'View saved QR',
+                  // textScaleFactor: 3,
+                ),
+              )), //button
+          Container(
+              margin: EdgeInsets.all(10),
+              child: FloatingActionButton(
+                onPressed: () {
+                  //action code for button 2
+                },
+                backgroundColor: Colors.deepPurpleAccent,
+                child: Text('View my QR'),
+              )), // button second
+
+          Container(
+              margin: EdgeInsets.all(10),
+              child: FloatingActionButton(
+                onPressed: () {
+                  //action code for button 3
+                },
+                backgroundColor: Colors.deepOrangeAccent,
+                child: Text('View my profile'),
+              )), // button third
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class Page {
+  final String title;
+  final IconData iconData;
+  Page(this.title, this.iconData);
+}
 
